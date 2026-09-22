2f6124007634e9264e5ccbf5cd34af3d97320516
ivanabregooo-web <ivanabregooo@gmail.com>
2026-09-22 01:12:34 -0600

    agregar archivos para docker compose y corregir gitignore para excluir .env

---
+++ entrega-Lab07/app.py
@@ -0,0 +1,46 @@
+import os
+import psycopg2
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def test_db_connection():
+    db_host = os.environ.get("DB_HOST", "db")
+    db_user = os.environ.get("DB_USER")
+    db_password = os.environ.get("DB_PASSWORD")
+    db_name = os.environ.get("DB_NAME")
+
+    try:
+        conn = psycopg2.connect(
+            host=db_host, user=db_user, password=db_password, database=db_name
+        )
+
+        cursor = conn.cursor()
+        cursor.execute("SELECT version();")
+        db_version = cursor.fetchone()[0]
+
+        cursor.close()
+        conn.close()
+
+        return jsonify(
+            {
+                "status": "success",
+                "message": "Conectado a la base de datos :)",
+                "database_version": db_version,
+            }
+        )
+
+    except Exception as e:
+        return jsonify(
+            {
+                "status": "error",
+                "message": "NO conectado a la base de datos :(",
+                "error_details": str(e),
+            }
+        ), 500
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
---
+++ entrega-Lab07/initdb/01-table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE cdm.ejemplo (
+	ejemplo1 SERIAL PRIMARY KEY,
+	ejemplo2 VARCHAR(50),
+	ejemplo3 DATE,
+	ejemplo4 NUMERIC,
+	ejemplo5 VARCHAR(50)
+);
